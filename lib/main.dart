56b2bd85cf5678dec988ca8dd14bfa9c38140fee
qiaoyunrui <shixi_yunrui@staff.weibo.com>
2018-03-05 19:38:33 +0800

    [Version] Text

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'ShoppingListItem.dart';
 
 void main() {
   runApp(new MaterialApp(
@@ -11,17 +10,67 @@ void main() {
 class ContainerDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Center(
-        child: new Container(
-      width: 300.0,
-      height: 400.0,
-      decoration: new BoxDecoration(
-          color: const Color(0xfffce5cd),
-          border: new Border.all(
-            color: const Color(0xff6d9eeb),
-            width: 8.0,
-          )),
-      child: new Text("Hello World")
-    ));
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("文本控件"),
+      ),
+      body: new Column(
+        children: <Widget>[
+          new Text(
+            "黑色删除线",
+            style: new TextStyle(
+              color: const Color(0xffff0000),
+              decoration: TextDecoration.lineThrough,
+              decorationColor: const Color(0xff000000),
+              fontSize: 25.0,
+            ),
+          ),
+          new Text(
+            "下划线",
+            style: new TextStyle(
+              color: const Color(0xffff9900),
+              decoration: TextDecoration.underline,
+              fontSize: 24.0,
+            ),
+          ),
+          new Text(
+            "虚线上划线 & 斜体",
+            style: new TextStyle(
+              decoration: TextDecoration.overline,
+              decorationStyle: TextDecorationStyle.dashed,
+              fontSize: 23.0,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          new Text(
+            "serif 字体",
+            style: new TextStyle(fontFamily: 'serif', fontSize: 26.0),
+          ),
+          new Text(
+            "monospace 字体 & 加粗",
+            style: new TextStyle(
+              fontFamily: 'monospace',
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          new Text(
+            "2行跨度",
+            style: new TextStyle(
+              color: const Color(0xff4a86e8),
+              fontSize: 25.0,
+              height: 2.0,
+            ),
+          ),
+          new Text(
+            "2个字母间隔",
+            style: new TextStyle(
+              fontSize: 24.0,
+              letterSpacing: 2.0,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
