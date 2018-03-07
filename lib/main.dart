b699012d5838a467cb0d6b85941745fc139a8967
qiaoyunrui <juhezix@163.com>
2018-03-07 10:18:08 +0800

    [Version] Expanded

--- lib/main.dart
+++ lib/main.dart
@@ -3,26 +3,44 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(new MaterialApp(
     title: "YiTiao",
-    home: new ImageDemo(),
+    home: new LayoutDemo(),
   ));
 }
 
-class ImageDemo extends StatelessWidget {
+class LayoutDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text(
-          "From URL",
-        ),
+        title: new Text("水平方向布局"),
       ),
-      body: new Center(
-        child: new Image.network(
-          "http://pic.baike.soso.com/p/20130828/20130828161137-1346445960.jpg",
-          scale: 2.0,
-        ),
+      body: new Column(
+        children: <Widget>[
+          new RaisedButton(
+            onPressed: () {
+              print("点红色按钮事件");
+            },
+            color: const Color(0xffcc0000),
+            child: new Text("红色按钮"),
+          ),
+          new Expanded(
+            child: new RaisedButton(
+              onPressed: () {
+                print("点击黄色按钮事件");
+              },
+              color: const Color(0xfff1c232),
+              child: new Text("黄色按钮"),
+            ),
+          ),
+          new RaisedButton(
+            onPressed: () {
+              print("点击粉色按钮事件");
+            },
+            color: const Color(0xffea9999),
+            child: new Text("粉色按钮"),
+          )
+        ],
       ),
     );
   }
 }
-//
