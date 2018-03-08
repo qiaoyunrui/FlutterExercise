72ea42a745bd208552378db56b88853051040772
qiaoyunrui <juhezix@163.com>
2018-03-08 10:57:24 +0800

    [Version] SizedBox & AspectRatio

--- lib/main.dart
+++ lib/main.dart
@@ -11,20 +11,17 @@ class LayoutDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        appBar: new AppBar(
-          title: new Text("水平方向布局"),
+      appBar: new AppBar(
+        title: new Text("水平方向布局"),
+      ),
+      body: new AspectRatio(
+        aspectRatio: 2.0 / 2.0,
+        child: new Container(
+          decoration: new BoxDecoration(
+            color: Colors.lightBlueAccent[100],
+          ),
         ),
-        body: new Stack(
-          children: <Widget>[
-            new Align(
-              alignment: new FractionalOffset(0.0, 0.0),
-              child: new Image.network("http://up.qqjia.com/z/25/tu32710_10.jpg"),
-            ),
-            new Align(
-              alignment: FractionalOffset.bottomRight,
-              child: new Image.network("http://up.qqjia.com/z/25/tu32710_11.jpg"),
-            )
-          ],
-        ));
+      ),
+    );
   }
 }
