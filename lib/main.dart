a13711f41bdac307017b783828ae21d62a7c2aa1
qiaoyunrui <juhezix@163.com>
2018-03-08 11:07:12 +0800

    [Version] DecoratedBox

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,17 @@ class LayoutDemo extends StatelessWidget {
       appBar: new AppBar(
         title: new Text("水平方向布局"),
       ),
-      body: new AspectRatio(
-        aspectRatio: 2.0 / 2.0,
-        child: new Container(
-          decoration: new BoxDecoration(
-            color: Colors.lightBlueAccent[100],
+      body: new DecoratedBox(
+        decoration: new BoxDecoration(
+          gradient: new LinearGradient(
+            colors: <Color>[const Color(0xff00ffff), const Color(0xffff6eb4)],
+            begin: const FractionalOffset(0.0, 0.0),
+            end: const FractionalOffset(1.0, 1.0),
           ),
+        ),
+        child: new Container(
+          width: 300.0,
+          height: 300.0,
         ),
       ),
     );
