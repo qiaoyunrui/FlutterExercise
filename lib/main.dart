7b1325aa60acf1e40ff9d933766166adf59a0a64
qiaoyunrui <juhezix@163.com>
2018-03-18 14:25:47 +0800

    [Version] PopupMenu

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(new MaterialApp(
     title: "YiTiao",
-    home: new LayoutDemo(),
+    home: new MenusDemo(),
   ));
 }
 
@@ -25,6 +25,46 @@ class LayoutDemo extends StatelessWidget {
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class MenusDemo extends StatefulWidget {
+  @override
+  _MenusDemoState createState() => new _MenusDemoState();
+}
+
+class _MenusDemoState extends State<MenusDemo> {
+  String _bodyStr = "显示菜单的点击";
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("MenuDemo"),
+        actions: <Widget>[
+          new PopupMenuButton(
+            itemBuilder: (BuildContext context) => <PopupMenuItem<String>>[
+                  new PopupMenuItem<String>(
+                    child: new Text("选项一"),
+                    value: "选项一的值",
+                  ),
+                  new PopupMenuItem<String>(
+                    child: new Text("选项二"),
+                    value: "选项二的值",
+                  )
+                ],
+            onSelected: (String value) {
+              setState(() {
+                _bodyStr = value;
+              });
+            },
+          )
+        ],
+      ),
+      body: new Center(
+        child: new Text(_bodyStr),
       ),
     );
   }
